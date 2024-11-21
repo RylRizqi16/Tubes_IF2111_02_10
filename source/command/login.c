f825ec4749e5cf2f5acf995c866c6bf0ab79891f
Fhatika Adhalisman Ryanjani <19623138@std.stei.itb.ac.id>
2024-11-21 16:05:37 +0700

    refactor: Changed header directory source

--- source/command/login.c
+++ source/command/login.c
@@ -1,5 +1,14 @@
 #include "login.h"
 
+int custom_strcmp(const char *str1, const char *str2) {
+    while (*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++;
+    }
+    return *(unsigned char *)str1 - *(unsigned char *)str2;
+}
+
+// Masuk sebagai pengguna yang telah terdaftar
 void loginUser(char username[MAX_LEN], char password[MAX_LEN]) {
     if (currentUserIndex != -1) {
         printf("Anda masih tercatat sebagai %s. Silakan LOGOUT terlebih dahulu.\n", userList.users[currentUserIndex].username);
@@ -11,10 +20,10 @@ void loginUser(char username[MAX_LEN], char password[MAX_LEN]) {
     printf("Password: ");
     scanf("%s", password);
 
-    // Cari pengguna
+// Cari pengguna
     for (int i = 0; i < userList.count; i++) {
-        if (strcmp(userList.users[i].username, username) == 0 &&
-            strcmp(userList.users[i].password, password) == 0) {
+        if (custom_strcmp(userList.users[i].username, username) == 0 &&
+            custom_strcmp(userList.users[i].password, password) == 0) {
             currentUserIndex = i;
             printf("Anda telah login ke PURRMART sebagai %s.\n", username);
             return;
--- source/command/login.h
+++ source/command/login.h
@@ -1,5 +1,7 @@
-#include <stdio.h>
-#include <string.h>
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <stdio.h> 
 
 #define MAX_USERS 100
 #define MAX_LEN 100
@@ -18,6 +20,8 @@ typedef struct {
 UserList userList;
 int currentUserIndex = -1;
 
+int custom_strcmp(const char *str1, const char *str2);
+
 void loginUser(char username[MAX_LEN], char password[MAX_LEN]);
 /* Melakukan login pengguna ke sistem.
    I.S. : userList mungkin kosong atau berisi pengguna terdaftar, 
@@ -26,3 +30,5 @@ void loginUser(char username[MAX_LEN], char password[MAX_LEN]);
           currentUserIndex menunjuk ke indeks pengguna yang login
           Jika username atau password salah, currentUserIndex tetap -1
           Jika ada pengguna lain yang sedang login, meminta pengguna untuk logout terlebih dahulu */
+
+#endif 
