afc85a4bfade0cf34941c9bc6c1075ec8a08f54b
Fhatika Adhalisman Ryanjani <19623138@std.stei.itb.ac.id>
2024-11-21 15:57:44 +0700

    refactor: Changed header directory source

--- source/command/register.c
+++ source/command/register.c
@@ -1,5 +1,22 @@
 #include "register.h"
 
+// Membandingkan dua string karakter demi karakter.
+int custom_strcmp(const char *str1, const char *str2) {
+    while (*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++;
+    }
+    return *(unsigned char *)str1 - *(unsigned char *)str2;
+    // Mengembalikan selisih nilai ASCII dari karakter pertama yang berbeda.
+}
+
+// Menyalin string dari src (sumber) ke dest (tujuan).
+void custom_strcpy(char *dest, const char *src) {
+    while ((*dest++ = *src++));
+    // Salin setiap karakter (termasuk karakter null '\0') dari src ke dest.
+}
+
+// Membuat akun baru
 void registerUser(char username[MAX_LEN], char password[MAX_LEN]) {
     if (userList.count >= MAX_USERS) {
         printf("User list is full, cannot register more users.\n");
@@ -28,18 +45,22 @@ void registerUser(char username[MAX_LEN], char password[MAX_LEN]) {
         }
     }
 
-    // Cek apakah username sudah ada
+    // Cek apa username sudah ada
     for (int i = 0; i < userList.count; i++) {
-        if (strcmp(userList.users[i].username, username) == 0) {
+        if (custom_strcmp(userList.users[i].username, username) == 0) {
             printf("Akun dengan username %s gagal dibuat. Silakan lakukan REGISTER ulang.\n", username);
-            return;
+            return; 
         }
     }
-    
-    // Tambahkan pengguna baru
-    strcpy(userList.users[userList.count].username, username);
-    strcpy(userList.users[userList.count].password, password);
-    userList.users[userList.count].money = 0; // Default money
+
+    // Menyalin username ke daftar pengguna (userList).
+    custom_strcpy(userList.users[userList.count].username, username);
+    // Menyalin password ke daftar pengguna (userList).
+    custom_strcpy(userList.users[userList.count].password, password);
+    // Inisialisasi money = 0 untuk pengguna baru.
+    userList.users[userList.count].money = 0;
+     // Menambah jumlah pengguna di userList.
     userList.count++;
+
     printf("Akun dengan username %s telah berhasil dibuat. Silakan LOGIN untuk melanjutkan.\n", username);
 }
--- source/command/register.h
+++ source/command/register.h
@@ -1,5 +1,7 @@
-#include <stdio.h>
-#include <string.h>
+#ifndef REGISTER_H
+#define REGISTER_H
+
+#include <stdio.h>  
 
 #define MAX_USERS 100
 #define MAX_LEN 100
@@ -18,10 +20,17 @@ typedef struct {
 UserList userList;
 int currentUserIndex = -1;
 
-void registerUser(char username[MAX_LEN], char password[MAX_LEN]); 
+int custom_strcmp(const char *str1, const char *str2);
+void custom_strcpy(char *dest, const char *src);
+
+void registerUser(char username[MAX_LEN], char password[MAX_LEN]);
 /* Mendaftarkan pengguna baru ke dalam sistem.
    I.S. : userList mungkin kosong atau sudah berisi beberapa pengguna
    F.S. : Jika username belum terdaftar, pengguna baru ditambahkan ke userList
           Jika username sudah terdaftar, tidak ada perubahan pada userList
           Jika userList penuh, tidak ada perubahan pada userList 
           User diminta memasukkan username dan password, HANYA boleh satu kata*/
+
+// Deklarasi fungsi manual (jika diperlukan di tempat lain)
+
+#endif 
