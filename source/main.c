fcb91fd880029896c80c31b910a24f115c0acb24
RylRizqi16 <18223084@std.stei.itb.ac.id>
2024-11-25 11:39:23 +0700

    refactor : main.c,feat : makefile

--- source/main.c
+++ source/main.c
@@ -3,11 +3,10 @@
 #include <math.h>
 #include <time.h>
 #include "command/load.h"
-#include "command/login.h"
-#include "command/logout.h"
+#include "command/login_logout.h"
 #include "command/register.h"
 #include "command/work.h"
-#include "command/workchallenge.h"
+#include "command/work_challenge.h"
 #include "command/store.h"
 #include "command/help.h"
 #include "command/save.h"
@@ -19,13 +18,18 @@
 #include "ADT/Mesin_Karakter/mesinkarakter.h"
 #include "ADT/Mesin_Kata/mesinkata.h"
 #include "ADT/QueueBarang/queue_barang.h"
-#include "ADT/User/user.h"
 
 
 int main() {
     char input[100];
-    Word currentCommand;
-    int session = 0;  //Welcome Menu, 1: Login Menu, 2: Main Menu
+    int session = 0;
+    ArrayBarang items = CreateArrayBarang();
+    ArrayUser users; 
+    QueueBarang request;
+    PenggunaSekarang PS;
+    createPenggunaSekarang(&PS);
+    CreateQueueBarang(&request);
+    CreateUserArray(&users);
 
     printf("Selamat datang di PURRMART! Ketik HELP untuk bantuan.\n");
 
@@ -34,36 +38,29 @@ int main() {
         ClearCurrentWord();
         switch (session) {
             case 0:
-                welcome_menu();
-                break;
+                printf("\n======================================[ Welcome Menu PURRMART ]====================================\n");
             case 1:
-                login_menu();
-                break;
+                printf("\n=======================================[ LOGIN Menu PURRMART ]=====================================\n");
             case 2:
-                main_menu();
-                break;
+                printf("\n========================================[ Main Menu PURRMART ]=====================================\n");
+
         }
 
         // Membaca Input
         printf(">> ");
         scanf("%s", input);
-
-        if (EndWord) {
-            printf("Tidak ada input yang diberikan.\n");
-            continue;
-        }
-        processCommand(input, &session);
+        processCommand(input, &session, &items, &users, &request, &PS);
     }
     return 0;
 }
 
 // Fungsi untuk mengolah command yang diberikan
-void processCommand(char command, int session) {
+void processCommand(char *command, int *session, ArrayBarang *items, ArrayUser *users, QueueBarang *request, PenggunaSekarang *PS) {
     if (BandingkanChar(command, "QUIT") == 1 || BandingkanChar(command, "quit") == 1) {
         char saveFile[50];
         printf("Masukkan nama file penyimpanan: ");
         scanf("%s", saveFile);
-        quit(saveFile);
+        quit(saveFile, &items, &users);
     } 
     else if (*session == 0) { // Welcome Menu
         if (BandingkanChar(command, "START") == 1 || BandingkanChar(command, "start") == 1) {
@@ -73,11 +70,7 @@ void processCommand(char command, int session) {
             char filename[50];
             printf("Masukkan nama file yang ingin di-load: ");
             scanf("%s", filename);
-
-            ArrayBarang items = CreateArrayBarang();
-            ArrayUser users = CreateUserArray();
-
-            load(filename, &itemCount, items, prices, &userCount, users, passwords, balances);
+            load(filename, &items, &users);
         }
         else if (BandingkanChar(command, "HELP") == 1 || BandingkanChar(command, "help") == 1) {
             welcome_menu();
@@ -90,20 +83,10 @@ void processCommand(char command, int session) {
         char username[100], password[100];
         if (command == "REGISTER" || command == "register") {
             if (BandingkanChar(command, "REGISTER") == 1 || BandingkanChar(command, "register") == 1) {
-                printf("Username: ");
-                scanf("%s", username);
-                printf("Password: ");
-                scanf("%s", password);
-                registerUser(username, password);
+                register_user(&users);
         }
         else if (BandingkanChar(command, "LOGIN") == 1 || BandingkanChar(command, "login") == 1) {
-            printf("Username: ");
-            scanf("%s", username);
-            printf("Password: ");
-            scanf("%s", password);
-            if (loginUser(username, password)) {
-                *session = 2;
-            }
+            login(&users, &PS);
         } 
         else if (BandingkanChar(command, "HELP") == 1 || BandingkanChar(command, "help") == 1) {
             login_menu();
@@ -118,20 +101,20 @@ void processCommand(char command, int session) {
         } else if (BandingkanChar(command, "WORKCHALLENGE") == 1 || BandingkanChar(command, "workchallenge") == 1) {
             workChallenge();
         } else if (BandingkanChar(command, "STORELIST") == 1 || BandingkanChar(command, "storelist") == 1) {
-            storeList();
+            storeList(&items);
         } else if (BandingkanChar(command, "STORE REQUEST") == 1 || BandingkanChar(command, "store request") == 1) {
-            storeRequest();
+            storeRequest(&items, &request);
         } else if (BandingkanChar(command, "STORE SUPPLY") == 1 || BandingkanChar(command, "store supply") == 1) {
-            storeSupply();
+            storeSupply(&items, &request);
         } else if (BandingkanChar(command, "STORE REMOVE") == 1 || BandingkanChar(command, "store remove") == 1) {
-            storeRemove();
+            storeRemove(&items);
         } else if (BandingkanChar(command, "LOGOUT") == 1 || BandingkanChar(command, "logout") == 1) {
-            logout();
-            *session = 1;
+            logout(&PS);
+            session = 1;
         } else if (BandingkanChar(command, "SAVE") == 1 || BandingkanChar(command, "save") == 1) {
             char filename[50];
             sscanf(command + 5, "%s", filename);
-            saveState(filename);
+            save(&filename,&items, &users);
         } else if (BandingkanChar(command, "HELP") == 1 || BandingkanChar(command, "help") == 1) {
             displayMainMenu();
         } else {
