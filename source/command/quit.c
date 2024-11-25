04563eb8fa7ace89b8c73cf01dbf9d39d6837b81
thaffariq <19623002@std.stei.itb.ac.id>
2024-11-25 20:56:39 +0700

    fix: Fixed a bug in quit

--- source/command/quit.c
+++ source/command/quit.c
@@ -3,23 +3,15 @@
 #include "quit.h"
 
 void quit(char *filename, ArrayBarang *barangToko, ArrayUser *users){
-    char response;
-
-    do {
-        printf("Apakah kamu ingin menyimpan data sesi sekarang (Y/N)? ");
-        START(NULL, "");
-        ADV();
-        response = GetCC();
-
+    printf("Apakah kamu ingin menyimpan data sesi sekarang (Y/N)? ");
+    START(NULL, "");
+    ADV();
+    char response = GetCC();
         if (response == 'Y' || response == 'y') {
             save(filename, barangToko, users); // Simpan data
             printf("Data berhasil disimpan. Kamu keluar dari PURRMART. Dadah ^_^/\n");
             exit(0);
-        } else if (response == 'N' || response == 'n') {
+        } else /*if (response == 'N' || response == 'n')*/ {
             printf("Kamu keluar dari PURRMART tanpa menyimpan. Dadah ^_^/\n");
             exit(0);
-        } else {
-            printf("Input tidak valid. Silakan masukkan Y atau N\n");
-        }
-    } while (response != 'Y' && response != 'y' && response != 'N' && response != 'n');
-}
+    }
