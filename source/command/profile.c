485e0fd6f7ff29a604bbd36d39004a77509be097
DwikyHD <dwiky414@gmail.com>
2024-12-20 09:26:00 +0700

    feat : profile
    add profile

---
+++ source/command/profile.c
@@ -0,0 +1,6 @@
+#include "profile.h"
+
+void profile(){
+    printf("Nama : %s!\n", username);
+    printf("Saldo: %d\n", PS->money);
+}
---
+++ source/command/profile.h
@@ -0,0 +1,8 @@
+#ifndef PROFILE_H
+#define PROFILE_H
+
+#include "../ADT/ArrayStatikUser/arrayuser.h"
+#include "../ADT/User/user.h"
+#include "login_logout.h"
+
+void profile();
